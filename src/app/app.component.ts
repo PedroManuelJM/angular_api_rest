ea92c9b90298a43bc3b6f4eda9e8d8a4a92f7d9d
Pedro Manuel Jurado Moreno <mpedrojurado@gmail.com>
2021-02-12 13:51:52 -0500

    subiendo imagen

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { RestService } from './rest.service';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,19 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'angular-api-rest';
+  public lista:any=[]
+  constructor(private RestService:RestService){  
+  }
+  ngOnInit():void{
+    this.cargarData();
+  }
+  public cargarData(){
+    this.RestService.get("https://pedrojm.000webhostapp.com/youtube/listavideos.php")
+    .subscribe(respuesta=>{
+      console.log(respuesta)
+      this.lista=respuesta;
+    })
+  }
+
+
 }
